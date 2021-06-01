92a847d79e6df8a92ef1294b905fed199749e397
David Owen <owend91@gmail.com>
2021-06-01 12:35:42 -0600

    Put the query parameters into cards to make UI a little nicer

--- frontend/src/components/QueryComponents/BrowserQueryParams.jsx
+++ frontend/src/components/QueryComponents/BrowserQueryParams.jsx
@@ -4,65 +4,58 @@ import Statistic from '../Statistics/StatisticComp'
 
 function BrowserQueryParams(props) {
     return (
-      <div>
-        <div className="row">
-        <div className="col-lg-6">
-          <h3>Characteristic Parameters</h3>
-          <div className="row pb-1">
-              <button className="btn btn-outline-primary" onClick={props.handleAddCharFields}> Add Property</button>
+      <div className='card'>
+        <div className="card-body">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Characteristic Parameters</h5>
+                  <div className="row pb-1">
+                    <button className="btn btn-outline-primary" onClick={props.handleAddCharFields}> Add Property</button>
+                  </div>
+                  <div className="row pt-1" style={{maxHeight: '15em', overflowY:'scroll', width:'100%'}}>
+                    {props.charFields.map( (char, index) => {
+                      return (
+                          <div class="row">
+                            <Characteristic width={'5em'} idx={index} removeChar={props.handleRemoveCharFields} inputChange={props.handleCharInputChange} value={char.value} characteristic={char.characteristic} />
+                          </div>   
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-        <div className="col-lg-6">
-          <h3>Statistic Parameters</h3>
-          <div className="row pb-1">
-              <button className="btn btn-outline-primary" onClick={props.handleAddStatFields}> Add Statistic</button>
-          </div>
-        </div>
-      </div>
-      <div className="row" style={{maxHeight: '15em', overflowY:'scroll', width:'100%'}}>
-          <div className="col-lg-6">
-            
-            {/* <h3>Characteristic Parameters</h3> */}
-
-
-            {props.charFields.map( (char, index) => {
-              return (
-                  <div class="row">
-                    <Characteristic width={'5em'} idx={index} removeChar={props.handleRemoveCharFields} inputChange={props.handleCharInputChange} value={char.value} characteristic={char.characteristic} />
-                  </div>   
-              )
-            })}
-            
-          </div>
-          <div className="col-lg-6">
-            {/* <h3>Statistic Parameters</h3> */}
-            
-            {/* <div className="row" style={{textAlign: 'center'}}>
-            <div className="form-check" onClick={props.updateSeasonCheck}>
-              <input className="" type="checkbox" checked={props.sameSeason} id="sameSeasonCheck" name="sameSeasonCheck" />
-              <label className="pl-1" for="sameSeasonCheck">
-                Same season
-              </label>
+            <div className="col-lg-6">
+              <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Statistic Parameters</h5>
+                    <div className="row pb-2" style={{textAlign: 'center'}}>
+                      <SeasonStatsHeader 
+                        sameseason={props.sameseason}
+                        updateSeasonCheck={props.updateSeasonCheck}
+                        idx={-99}
+                        width={'5em'}
+                        statyearval={props.statyearval}
+                        inputChange={props.handleStatInputChange}
+                        statteam={props.statteam}
+                      />
+                    </div>
+                    <div className="row pb-1">
+                      <button className="btn btn-outline-primary" onClick={props.handleAddStatFields}> Add Statistic</button>
+                    </div>
+                    <div className="row pt-1" style={{maxHeight: '15em', overflowY:'scroll', width:'100%'}}>
+                        {props.statFields.map( (stat, index) => {
+                          return (
+                              <div class="row">
+                                <Statistic idx={index} removeStat={props.handleRemoveStatFields} inputChange={props.handleStatInputChange} value={stat.value} statistic={stat.statistic} comparator={stat.comparator} />
+                              </div>   
+                          )
+                        })}
+                    </div>
+                  </div>
+                </div>
             </div>
-            </div> */}
-            <div className="row" style={{textAlign: 'center'}}>
-            <SeasonStatsHeader 
-              sameseason={props.sameseason}
-              updateSeasonCheck={props.updateSeasonCheck}
-              idx={-99}
-              width={'5em'}
-              statyearval={props.statyearval}
-              inputChange={props.handleStatInputChange}
-              statteam={props.statteam}
-            />
-            </div>
-            {props.statFields.map( (stat, index) => {
-              return (
-                  <div class="row">
-                    <Statistic idx={index} removeStat={props.handleRemoveStatFields} inputChange={props.handleStatInputChange} value={stat.value} statistic={stat.statistic} comparator={stat.comparator} />
-                  </div>   
-              )
-            })}
           </div>
         </div>
       </div>
